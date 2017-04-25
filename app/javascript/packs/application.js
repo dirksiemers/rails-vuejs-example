0d03609cf3712053e18c50e46cf4903380e45b57
Dirk Siemers <dirk.siemers@gmail.com>
2017-04-25 19:04:05 +0200

    cleanup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,37 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import Vue from 'vue/dist/vue.esm'
+import _ from 'underscore'
+import marked, { Renderer } from 'marked';
+import highlightjs from 'highlight.js';
+import 'highlight.js/styles/railscasts.css';
 
-console.log('Hello World from Webpacker')
+document.addEventListener('DOMContentLoaded', () => {
+  const renderer = new Renderer();
+  renderer.code = (code, language) => {
+    // Check whether the given language is valid for highlight.js.
+    const validLang = !!(language && highlightjs.getLanguage(language));
+    // Highlight only if the language is valid.
+    const highlighted = validLang ? highlightjs.highlight(language, code).value : code;
+    // Render the highlighted code with `hljs` class.
+    return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
+  };
+  marked.setOptions({ renderer });
+
+  const app = new Vue({
+    el: '#editor',
+    data: {
+      input: '# Hello World\n\n```ruby\nclass HelloWorld\n  def initialize(name)\n    @name = name.capitalize\n  end\n  def sayHi\n    puts "Hello #{@name}!"\n  end\nend\n\nhello = HelloWorld.new("World")\nhello.sayHi\n```'
+    },
+    computed: {
+      compiledMarkdown: function () {
+        return marked(this.input, { sanitize: true })
+      }
+    },
+    methods: {
+      update: _.debounce(function (e) {
+        this.input = e.target.value
+      }, 300)
+    }
+  })
+
+  console.log(app)
+})
